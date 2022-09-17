7b410007889dd34c19677ed1e83c616964d7a1ab
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2022-09-17 10:13:03 +0200

    tag

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,20 +4,47 @@ using UnityEngine;
 
 public class Player : Entity
 {
-    private void Start()
-    {
-        sprite = GetComponent<Sprite>();
-        col = GetComponent<Collider2D>();
-        rb = GetComponent<Rigidbody2D>();
-    }
-    private void FixedUpdate()
-    {
-        Move();
-    }
-    protected override void Move()
-    {
-        Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        rb.MovePosition(transform.position + dir.normalized * Time.fixedDeltaTime * speed); 
+	public LayerMask mask;
+    
+	private void Start()
+	{
+		sprite = GetComponent<Sprite>();
+		col = GetComponent<Collider2D>();
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Shoot();
+		}
+	}
+	private void FixedUpdate()
+	{
+		Move();
+	}
+	protected override void Move()
+	{
+		Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		rb.MovePosition(transform.position + dir.normalized * Time.fixedDeltaTime * speed); 
+	}
+
+	private void Shoot()
+	{
+		Vector3 dir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+		RaycastHit2D rc = Physics2D.Raycast(transform.position, dir, 10.0f, mask);
+		Debug.DrawRay(transform.position, dir * 10.0f, Color.red, 2.0f);
+		Debug.Log(rc.transform.name);
+        if (rc.collider != null)
+        {
+			Debug.Log("Enemy detected");
+			if (rc.collider.GetComponent<Entity>() != null)
+            {
+				
+                //rc.collider.GetComponent<Entity>().TakeDamage(statistic.attack);
+            }
+        }
     }
 
 }
