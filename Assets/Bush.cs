cea94c16ca096c1e5a2d8aa29ddb098715b360b7
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2022-10-10 10:16:18 +0200

    order in layer

---
+++ Assets/Bush.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bush : MonoBehaviour
+{
+    private BoxCollider collider;
+    private SpriteRenderer spriteRenderer;
+    private SpriteRenderer otherSPR;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bestiole")
+        {
+            Debug.Log("enter");
+            Debug.Log(other.gameObject.name);
+            otherSPR = other.gameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.sortingOrder = otherSPR.sortingOrder + 1;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Bestiole")
+        {
+            Debug.Log("exit");
+            spriteRenderer.sortingOrder = otherSPR.sortingOrder - 1;
+        }
+
+    }
+}
