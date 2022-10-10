b61ad818478141b0d8ff0ea15b7cc62ad1a60457
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2022-10-10 10:38:34 +0200

    bush maj animation et layer

--- Assets/Bush.cs
+++ Assets/Bush.cs
@@ -7,14 +7,17 @@ public class Bush : MonoBehaviour
     private BoxCollider collider;
     private SpriteRenderer spriteRenderer;
     private SpriteRenderer otherSPR;
+    private Animator animator;
     private void Start()
     {
+        animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Bestiole")
         {
+            animator.SetTrigger("Anim");
             Debug.Log("enter");
             Debug.Log(other.gameObject.name);
             otherSPR = other.gameObject.GetComponent<SpriteRenderer>();
