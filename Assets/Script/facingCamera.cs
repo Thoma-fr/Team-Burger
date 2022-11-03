19c0794bf9a5bdba71261811b3bb5d34656dff11
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2022-11-03 13:03:27 +0100

    modif sur les arbres

--- Assets/Script/facingCamera.cs
+++ Assets/Script/facingCamera.cs
@@ -11,9 +11,14 @@ public class facingCamera : MonoBehaviour
     private int baseORder;
     public bool hasRotate;
     private Transform rot;
+    public GameObject matPlane;
+    public GameObject spriteImage;
     // Start is called before the first frame update
     void Start()
     {
+        if(matPlane!= null)
+            matPlane.SetActive(false);
+        
         GameManager.instance.faceTheCam.Add(gameObject);
         //baseORder = GetComponent<SpriteRenderer>().sortingOrder;
     }
@@ -36,12 +41,22 @@ public class facingCamera : MonoBehaviour
 
             //transform.localRotation = a.transform.rotation;
             //transform.DOLocalRotate(new Vector3(transform.localRotation.x, transform.localRotation.y, transform.localRotation.z-20), 1f);
+            if (matPlane != null)
+            {
+                matPlane.SetActive(true);
+                spriteImage.GetComponent<SpriteRenderer>().enabled = false;
+            }
             transform.DOLocalRotateQuaternion(a.transform.rotation, 1f).SetEase(Ease.OutBounce).OnComplete(() => hasRotate = true);
             transform.position = new Vector3(transform.position.x, transform.position.y, -0.07f);
             hasRotate = true;
         }
         else
         {
+            if (matPlane != null)
+            {
+                matPlane.SetActive(false);
+                spriteImage.GetComponent<SpriteRenderer>().enabled = true;
+            }
             Debug.Log("func2");
             transform.DORotate(new Vector3(0, 0, 0), 0f).OnComplete(() => hasRotate = false);
             transform.position = new Vector3(transform.position.x, transform.position.y, -0.02f);
