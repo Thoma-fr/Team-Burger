aa69c3394214de00c4cc799eec4c3276d44dee75
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2022-11-24 09:09:37 +0100

    oui

--- Assets/pantagram.cs
+++ Assets/pantagram.cs
@@ -8,29 +8,31 @@ using Unity.VisualScripting;
 public class pantagram : MonoBehaviour
 {
     private Volume Volume;
+    public VolumeProfile Profile;
     private Animator anim;
     private void Start()
     {
         Volume = GetComponent<Volume>();
         anim = GetComponent<Animator>();
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         
         if (other.CompareTag("Player"))
         {
             Debug.Log("hey");
             DOTween.To(() => Volume.weight, x => Volume.weight = x, 1f, 1f);
-            anim.enabled = true;
+            anim.SetTrigger("On");
 
         }
     }
-    private void OnTriggerExit(Collider other)
+
+    private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             DOTween.To(() => Volume.weight, x => Volume.weight = x, 0f, 0.5f);
-            anim.enabled = false;
+            
         }
     }
 }
